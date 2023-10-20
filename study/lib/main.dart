c83a3675f2386bcc206aec515a93a22ef5e362ea
Dcool <90443691+Leesiwon12171260@users.noreply.github.com>
2023-10-20 23:43:37 +0900

    기본연습

---
+++ study/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:study/common/component/custom_text_form_field.dart';
+
+void main() {
+  runApp(_App());
+}
+class _App extends StatelessWidget {
+  //상속을 받았고 아래것은 생성자다
+  _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              hintText: '이메일을 입력해주세요.',
+              onChanged: (String value) {},
+            ),
+            SizedBox(height: 20),
+            CustomTextFormField(
+              hintText: '비밀번호를 입력해주세요.',
+              onChanged: (String value) {},
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
